c509bcc027477fb0a031fbb681c0d8d7070cda74
Green Wang <cocowool@gmail.com>
2019-03-15 21:54:32 +0800

    modify entity

--- vmeeting/src/main/java/com/edulinks/vmeeting/bean/Meeting.java
+++ vmeeting/src/main/java/com/edulinks/vmeeting/bean/Meeting.java
@@ -1,19 +1,36 @@
 package com.edulinks.vmeeting.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Meeting{
-    private final long id;
-    private final String name;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
 
-    public Meeting(long id, String name){
-        this.id = id;
-        this.name = name;
-    }
+    private Integer id;
+    private String name;
 
-    public long getId(){
+    // public Meeting(long id, String name){
+    //     this.id = id;
+    //     this.name = name;
+    // }
+
+    public Integer getId(){
         return id;
     }
 
     public String getName(){
         return name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
     }
 }
