26fe3cc451e8b4b6f035577eec1f962270d4ad8e
Green Wang <cocowool@qq.com>
2019-03-21 19:37:40 +0800

    add delete method

--- vmeeting/src/main/java/com/edulinks/vmeeting/user/UserController.java
+++ vmeeting/src/main/java/com/edulinks/vmeeting/user/UserController.java
@@ -6,11 +6,14 @@ import com.edulinks.vmeeting.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @RestController
 public class UserController {
@@ -32,7 +35,7 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @RequestMapping("user/update")
+    @RequestMapping("/user/update")
     public String updateUser(@RequestParam Integer id, @RequestParam String name){
         User n = new User();
         n.setId(id);
@@ -42,4 +45,13 @@ public class UserController {
 
         return "updated";
     }
+
+    @RequestMapping(value="/user/delete/{id}", method=RequestMethod.GET)
+    public String delete(@PathVariable("id") String id) {
+        userRepository.deleteById(Integer.parseInt(id));
+
+        return "deleted";
+        // return new SomeData();
+    }
+    
 }
---
+++ vmeeting/src/main/java/com/edulinks/vmeeting/user/UserService.java
@@ -0,0 +1,10 @@
+package com.edulinks.vmeeting.user;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+}
