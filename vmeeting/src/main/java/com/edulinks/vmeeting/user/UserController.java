0ef7e74aa72b15b1f5bfa404562fa58850a3a4a1
Green Wang <cocowool@qq.com>
2019-03-22 19:50:46 +0800

    remember postman

--- vmeeting/src/main/java/com/edulinks/vmeeting/user/UserController.java
+++ vmeeting/src/main/java/com/edulinks/vmeeting/user/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,13 +23,16 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/user/add")
-    public String addNewUser(@RequestParam String name ) {
-        User n = new User();
-        n.setName(name);
-        userRepository.save(n);
+    @PostMapping("/user/add")
+    public @ResponseBody User addNewUser(@RequestBody User user ) {
+        userRepository.save(user);
 
-        return "ok";
+        return user;
+        // User n = new User();
+        // n.setName(name);
+        // userRepository.save(n);
+
+        // return "ok";
     }
 
     @RequestMapping("/user")
