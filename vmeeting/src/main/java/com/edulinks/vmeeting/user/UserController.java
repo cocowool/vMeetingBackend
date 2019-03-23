f4e408fa18ec4031051236d944da91b58d5ee86d
Green Wang <cocowool@gmail.com>
2019-03-23 23:23:43 +0800

    got put method

--- vmeeting/src/main/java/com/edulinks/vmeeting/user/UserController.java
+++ vmeeting/src/main/java/com/edulinks/vmeeting/user/UserController.java
@@ -10,11 +10,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +55,21 @@ public class UserController {
         
         // return userRepository.findById( Integer.parseInt(id) );
     }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<Object> updateUser(@RequestBody User user, @PathVariable Integer id){
+        Optional<User> userOptional = userRepository.findById(id);
+
+        if( !userOptional.isPresent() ){
+            return ResponseEntity.notFound().build();
+        }
+
+        user.setId(id);
+        userRepository.save(user);
+
+        return ResponseEntity.noContent().build();
+    }
+
 
     @RequestMapping("/user/update")
     public String updateUser(@RequestParam Integer id, @RequestParam String name){
